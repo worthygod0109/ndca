95aae7c5ed321bcf0aebb0de92469d40b157ac3b
Pranav Pagare <pranav01092002@gmail.com>
2024-10-16 23:04:40 +0530

    sql p33o2

--- Server.js
+++ Server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const cors = require("cors");
 const multer = require("multer");
 const path = require("path");
